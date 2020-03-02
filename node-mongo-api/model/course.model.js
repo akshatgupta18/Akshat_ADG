cc1c0b91c3857291c132c7c3611eb35ccde04c4d
akshatgupta18 <60574865+akshatgupta18@users.noreply.github.com>
2020-03-03 00:02:20 +0530

    Update course.model.js

--- node-mongo-api/model/course.model.js
+++ node-mongo-api/model/course.model.js
@@ -1,21 +1,19 @@
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 
-var CourseSchema=new mongoose.Schema({
-    courseName:{
-        type:String,
-        required:"required"
+var CourseSchema = new mongoose.Schema({
+    courseName : {
+        type : String,
+        required : "Required"
     },
-    courseId:{
+    courseId : {
+        type : String
+    },
+    courseDuration : {
         type:String
     },
-    courseDuration:{
-        type:String
+    courseFee : {
+        type : String
     },
-    courseFee:{
-        type:String
-    },
-
 });
 
-mongoose.model("Course",CourseSchema);//the collection Course now will take
- //record only in this particular schema.
+mongoose.model("Course",CourseSchema)
